b9696ebe8f5d67e2efc6fc0ecbe9c4d9378654fd
zouzu <zl.uzunova@gmail.com>
2014-11-17 21:02:31 +0200

    TestMagnet

---
+++ Assets/Scripts/PMovePull.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PMovePull : MonoBehaviour
+{
+
+	//public Transform PlayerStartPoint;
+	public GameObject Player;
+	public GameObject Magnet;
+
+	public float speed;
+
+	public float timeLeftGreen;
+
+	public bool MClickOver;
+	public bool MagnetAbility;
+	
+	private string GreenCapsule;
+
+	public Rigidbody otherBody;
+
+	public Material GreenMaterial;
+	public Material WhiteMaterial;
+
+	void Start ()
+	{
+		Player = GameObject.FindGameObjectWithTag("Player");
+		//Magnet = GameObject.FindGameObjectWithTag("Magnet");
+
+		GreenCapsule = "GreenCapsule";
+
+		speed = 5f;
+
+		MClickOver = false;
+		MagnetAbility = false;
+	}
+	void Update()
+	{		
+		
+		if(MagnetAbility)	
+		{
+			timeLeftGreen -= Time.deltaTime;
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				Debug.Log("Pressed left click.");
+				CastRay();
+			}
+
+			if (MClickOver)
+			{
+				MagnitUpdate();
+			}
+
+
+			if ( timeLeftGreen < 0 )
+			{
+				DeactivateMagnetAbility();
+			}
+		}
+		
+	}
+
+	void DeactivateMagnetAbility()
+	{
+		MagnetAbility = false;
+		Player.rigidbody.useGravity = true;
+		Player.renderer.material = WhiteMaterial;
+	}
+	
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == GreenCapsule)
+		{
+			GreenCapsuleTriggerEnter(other.gameObject);
+		}
+		if (MClickOver)
+		{
+			if(other.gameObject.tag == Magnet.tag)
+			{
+				MagnetTriggerEnter();
+			}
+		}
+	}
+
+	void GreenCapsuleTriggerEnter(GameObject greenCapsuleObject)
+	{
+		MagnetAbility = true;
+		timeLeftGreen = 5f;
+		Player.renderer.material = GreenMaterial;
+		Destroy(greenCapsuleObject);
+	}
+
+	void MagnetTriggerEnter()
+	{
+		Player.transform.Translate(new Vector3 (0,0,0));
+		MClickOver = false;
+	}
+	
+	void CastRay() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 100))
+		{
+			Debug.DrawLine(ray.origin, hit.point);
+			Debug.Log("Hit object: " + hit.collider);
+			if (hit.collider.gameObject.tag.ToString() == "Magnet")
+			{
+				Debug.Log("Magnetizirahme se :))");
+				Player.rigidbody.useGravity = false;
+				Magnet = hit.collider.gameObject;
+				MClickOver = true;
+			}
+		}
+	}
+
+	void MagnitUpdate()
+	{
+		Vector3 whereIWantToBe = Magnet.transform.position;
+		Vector3 direction = whereIWantToBe - Player.transform.position;
+
+		Player.transform.Translate(direction * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x - speed;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x + speed;
+			this.transform.position = position;
+		}
+
+	
+	}
+
+//	void FixedUpdate()
+//	{
+//		float moveHorizontal = Input.GetAxis("Vertical");
+//		float moveVertical = Input.GetAxis("Horizontal");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//
+//		rigidbody.AddForce(movement * speed * Time.deltaTime);
+//	}
+
+}
