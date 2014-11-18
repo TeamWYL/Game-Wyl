4bfe0258f7a11cdc4ec8a17e7e30ac4212fbd23a
petkata <petyr_yontchev@abv.bg>
2014-11-18 12:14:58 +0200

    Added Indestructibility + timer script

---
+++ Assets/Scripts/Indestructibility.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Indestructibility : MonoBehaviour {
+	public GameObject blades;
+	public GameObject indestr;
+	public bool isIndestr;
+	public float timeLeftRed;
+	// Use this for initialization
+	void Start () {
+		blades.GetComponent<BoxCollider>().isTrigger = true;
+		timeLeftRed = 5f;
+		isIndestr = false;
+		indestr.renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isIndestr) {
+			timeLeftRed -= Time.deltaTime;
+			if (timeLeftRed<=0) {
+				isIndestr = false;
+				blades.GetComponent<BoxCollider>().isTrigger = true;
+
+			}
+				}
+	}
+	void OnColliderEnter (){
+		}
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "IndestructibilityPill") {
+			Destroy(col.gameObject);
+			blades.GetComponent<BoxCollider>().isTrigger = false;
+			isIndestr = true;
+				}
+		else {
+			//implemet health loss
+			Destroy (gameObject);
+				}
+
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//Attach in MainCamera
+public class Timer : MonoBehaviour {
+	public Slider slider;
+	public GameObject player;
+	public Image fill;
+
+	// Use this for initialization
+	void Awake (){
+		slider.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//uncomment which one you want to try
+		//magnit
+//		if (player.GetComponent<PMovePull> ().MagnetAbility) {
+//			fill.GetComponent<Image>().color = Color.green;
+//			slider.gameObject.SetActive (true);
+//			slider.value = player.GetComponent<PMovePull> ().timeLeftGreen;
+//		}
+		//indestr
+		if (player.GetComponent<Indestructibility> ().isIndestr) {
+			fill.GetComponent<Image>().color = Color.red;
+			slider.gameObject.SetActive (true);
+			slider.value = player.GetComponent<Indestructibility> ().timeLeftRed;
+		}
+		else {
+			slider.gameObject.SetActive (false);
+				}
+	}
+}
